f8b528161d616c7a4b12d00a1b2de6b647304b36
Aleph Melo <alephmelo@icloud.com>
2019-10-02 17:54:04 +0200

    Add more tests for helpers module.

--- test/util/test_helpers.py
+++ test/util/test_helpers.py
@@ -1,9 +1,15 @@
 import os
+from unittest.mock import MagicMock
+
+import pytest
 from discord import File
 
 from util.helpers import (
     create_discord_file_object,
     generate_image_search_url,
+    get_json_field_from_url,
+    get_json_fields_from_url,
+    mention,
 )
 
 
@@ -18,8 +24,7 @@ def test_generate_image_search_url():
         f"q={'this is a test'}&"
         f"searchType=image"
     )
-    if not query == google_image_query_url:
-        raise AssertionError()
+    assert query == google_image_query_url
 
 
 def test_generate_image_search_url_gif():
@@ -34,23 +39,85 @@ def test_generate_image_search_url_gif():
         f"searchType=image&"
         f"fileType=gif"
     )
-    if not query == google_image_query_url:
-        raise AssertionError()
+    assert query == google_image_query_url
 
 
 def test_create_discord_file_object():
     """Test create discord file object"""
-    img_link = 'https://media.discordapp.net/attachments/615527031148642334/627764848821796864/tano.png'
+    img_link = "https://media.discordapp.net/attachments/615527031148642334/627764848821796864/tano.png"
     discord_file = create_discord_file_object(img_link)
 
     assert type(discord_file) is File
-    assert 'SPOILER_' not in discord_file.filename
+    assert "SPOILER_" not in discord_file.filename
 
 
 def test_create_discord_file_object_as_spoiler():
     """Test create discord file object"""
-    img_link = 'https://media.discordapp.net/attachments/615527031148642334/627764848821796864/tano.png'
-    discord_file = create_discord_file_object(img_link, '--spoiler')
+    img_link = "https://media.discordapp.net/attachments/615527031148642334/627764848821796864/tano.png"
+    discord_file = create_discord_file_object(img_link, "--spoiler")
 
     assert type(discord_file) is File
-    assert 'SPOILER_' in discord_file.filename
+    assert "SPOILER_" in discord_file.filename
+
+
+def test_get_json_fields_from_url():
+    """Test get json fields from url"""
+    expected_json_data = [
+        {
+            "id": 1,
+            "name": "cerulean",
+            "year": 2000,
+            "color": "#98B2D1",
+            "pantone_value": "15-4020",
+        }
+    ]
+
+    json_data = get_json_fields_from_url("https://reqres.in/api/products/1", "data")
+    assert expected_json_data == json_data
+
+
+def test_get_json_field_from_url():
+    """Test get json field from url"""
+    expected_json_data = {
+        "id": 1,
+        "name": "cerulean",
+        "year": 2000,
+        "color": "#98B2D1",
+        "pantone_value": "15-4020",
+    }
+
+    assert expected_json_data == get_json_field_from_url(
+        "https://reqres.in/api/products/1", "data"
+    )
+
+
+def test_get_json_fields_from_url_failing():
+    """Test failling get json fields from url"""
+    with pytest.raises(Exception) as e:
+        assert get_json_fields_from_url()
+
+
+@pytest.fixture
+def ctx_object():
+    """Fixture to mock discord context objects"""
+    ctx = MagicMock()
+    member_1 = MagicMock()
+    member_2 = MagicMock()
+    member_1.name = "abc"
+    member_2.name = "def"
+    member_1.mention = "@abc"
+    member_2.mention = "@def"
+    ctx.message.channel.members = [member_1, member_2]
+    return ctx
+
+
+def test_mention_less_than_3_chars(ctx_object):
+    """Test mention with less than 3 chars"""
+    expected_result = "Don't be evil."
+    criteria = "ab"
+    assert mention(ctx_object, criteria) == expected_result
+
+
+def test_mention(ctx_object):
+    """Test mention using magic mock object"""
+    assert mention(ctx_object, "def") == "@def "
