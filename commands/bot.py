ba0ee03e082853d5ffabd6d91695443700c6be2b
ad3lm0 <ad3lm0@gmail.com>
2019-10-08 23:08:23 +0100

    grep in listall

--- commands/bot.py
+++ commands/bot.py
@@ -6,13 +6,27 @@ from util.helpers import format_params
 
 
 @command(desc="List all commands")
-async def listall(ctx):
+async def listall(ctx, grep=None):
 
     commands = Commands.get_instance().dictionary
     embed = Embed(title="Commands list", color=0x00ff75)
 
-    for cmd in sorted(commands.keys()):
-        embed.add_field(name=".{} {}".format(cmd, format_params(commands[cmd]['params'])),
-                        value=commands[cmd]['description'], inline=False)
+    if grep == None:
+
+        for cmd in sorted(commands.keys()):
+            embed.add_field(name=".{} {}".format(cmd, format_params(commands[cmd]['params'])),
+                            value=commands[cmd]['description'], inline=False)
+    
+    else:
+        filtered_commands = {key:value for key, value in commands.items() if str.lower(grep) in key.lower()}
+
+        if len(filtered_commands) < 1:
+            embed.add_field(name="achei óh",
+                            value="varios nadas...", inline=False)
+
+        else:    
+            for cmd in sorted(filtered_commands.keys()):
+                embed.add_field(name=".{} {}".format(cmd, format_params(commands[cmd]['params'])),
+                                value=commands[cmd]['description'], inline=False)
 
     await ctx.send(embed=embed)
