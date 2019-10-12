32aaf645267fca502ae300de58f9638424f8c3db
Aleph Melo <alephmelo@icloud.com>
2019-10-12 19:02:11 +0200

    Add ping test.

---
+++ test/commands/test_misc.py
@@ -0,0 +1,10 @@
+import pytest
+from commands.misc import ping
+from asyncmock import AsyncMock
+
+
+@pytest.mark.asyncio
+async def test_ping():
+    ctx = AsyncMock()
+    await ping(ctx)
+    ctx.send.assert_called_with("pong")    
