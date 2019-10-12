3878505074a0dc5deb2578821604536bed0e0368
Aleph Melo <alephmelo@icloud.com>
2019-10-12 19:22:39 +0200

    Add ping with mention test.

--- test/commands/test_misc.py
+++ test/commands/test_misc.py
@@ -1,5 +1,6 @@
-import pytest
 from commands.misc import ping
+
+import pytest
 from asyncmock import AsyncMock
 
 
@@ -8,3 +9,10 @@ async def test_ping():
     ctx = AsyncMock()
     await ping(ctx)
     ctx.send.assert_called_with("pong")
+
+
+@pytest.mark.asyncio
+async def test_ping_with_mention():
+    ctx = AsyncMock()
+    await ping(ctx, arg="ab")
+    ctx.send.assert_called_with("Pinging Don't be evil. 🏓")
