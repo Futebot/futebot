2f95897bcd58220105efb39fb4539b15367a4fa1
Fabricio Augusto <fabricionaweb@gmail.com>
2019-10-18 23:31:34 +0100

    split listall commands

--- commands/bot.py
+++ commands/bot.py
@@ -2,17 +2,22 @@ from discord import Embed
 
 from annotation.futebot import command
 from util.commands import Commands
-from util.helpers import format_params
+from util.helpers import format_params, split_dict
 
 
 @command(desc="List all commands")
 async def listall(ctx):
 
-    commands = Commands.get_instance().dictionary
-    embed = Embed(title="Commands list", color=0x00ff75)
+    embed_limit = 25
+    commands = split_dict(Commands.get_instance().dictionary, embed_limit)
 
-    for cmd in sorted(commands.keys()):
-        embed.add_field(name=".{} {}".format(cmd, format_params(commands[cmd]['params'])),
-                        value=commands[cmd]['description'], inline=False)
+    for page in commands:
+        embed = Embed(title="Commands list", color=0x00ff75)
 
-    await ctx.send(embed=embed)
+        for line in page:
+            command = page[line]
+            embed.add_field(name=".{} {}".format(line, format_params(command['params'])),
+                            value=command['description'],
+                            inline=False)
+
+        await ctx.send(embed=embed)
