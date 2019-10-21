b20f4df6b759291cc61d6dd83b17ef2fd5fbec30
Igo Lapa <igo.lapa@harveynichols.com>
2019-10-21 10:00:22 +0100

    feat: remove tiny url in favour of lmgtfy search
    test: make test work
    
    update test copy

--- test/commands/test_search.py
+++ test/commands/test_search.py
@@ -12,6 +12,5 @@ def ctx():
 
 @pytest.mark.asyncio
 async def test_lmgtfy(ctx):
-    expected_lmgtfy_url = "http://tinyurl.com/yyxa8vpq"
-    await lmgtfy(ctx, "bosta")
-    ctx.send.assert_called_with("http://tinyurl.com/yyxa8vpq")
+    await lmgtfy(ctx, "League of Legends")
+    ctx.send.assert_called()
