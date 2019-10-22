ddd7fec70e4adbaf68b1f9bdf09598226e251c62
Fabricio Augusto <fabricionaweb@gmail.com>
2019-10-22 01:23:42 +0100

    Specs for rm custom commands

--- test/commands/test_custom.py
+++ test/commands/test_custom.py
@@ -3,7 +3,8 @@ import os
 import pytest
 from asyncmock import AsyncMock
 
-from commands.custom import listcustom, add, c
+from commands.custom import listcustom, add, rm, c
+from service.command_suggestion_service import is_custom_command
 
 
 @pytest.fixture
@@ -26,7 +27,53 @@ async def test_list_custom(ctx):
 async def test_add(ctx):
     os.environ["COMMANDS_DATA_FILE"] = os.path.dirname(os.path.abspath(__file__)) + "/../data_test_add.yml"
 
-    await add(ctx, "a", "a")
-    await c(ctx, "a")
-    ctx.send.assert_called_with("a")
+    await add(ctx, "cmd", "value")
+    assert is_custom_command("cmd") is True
+
+    await c(ctx, "cmd")
+    ctx.send.assert_called_with("value")
+
+    os.remove(os.environ["COMMANDS_DATA_FILE"])
+
+
+@pytest.mark.asyncio
+async def test_rm(ctx):
+    os.environ["COMMANDS_DATA_FILE"] = os.path.dirname(os.path.abspath(__file__)) + "/../data_test_rm.yml"
+
+    await add(ctx, "cmd", "value")
+    assert is_custom_command("cmd") is True
+
+    await rm(ctx, "cmd")
+    assert is_custom_command("cmd") is False
+
+    os.remove(os.environ["COMMANDS_DATA_FILE"])
+
+
+@pytest.mark.asyncio
+async def test_rm_invalid_arg(ctx):
+    os.environ["COMMANDS_DATA_FILE"] = os.path.dirname(os.path.abspath(__file__)) + "/../data_test_rm.yml"
+
+    await add(ctx, "cmd", "value")
+    await rm(ctx, "")
+    assert str(ctx.send.call_args[0][0]) == "Are you dumb?"
+
+    os.remove(os.environ["COMMANDS_DATA_FILE"])
+
+
+@pytest.mark.asyncio
+async def test_rm_invalid_data(ctx):
+    os.environ["COMMANDS_DATA_FILE"] = os.path.dirname(os.path.abspath(__file__)) + "/../data_test_rm.yml"
+
+    await rm(ctx, "cmd")
+    ctx.send.assert_called_with("buguei")
+
+
+@pytest.mark.asyncio
+async def test_rm_invalid_cmd(ctx):
+    os.environ["COMMANDS_DATA_FILE"] = os.path.dirname(os.path.abspath(__file__)) + "/../data_test_rm.yml"
+
+    await add(ctx, "cmd", "value")
+    await rm(ctx, "invalid_cmd")
+    ctx.send.assert_called_with("Tem esse comando ai não")
+
     os.remove(os.environ["COMMANDS_DATA_FILE"])
