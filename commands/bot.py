1f0916b1cd7c9776a505f4b33ee4626ef90962c5
Flávio Mendes <flavio.mendes@rockalltech.com>
2019-10-29 09:35:33 +0000

    Undoing stuff

--- commands/bot.py
+++ commands/bot.py
@@ -6,19 +6,17 @@ from util.helpers import format_params, split_dict
 from .config import DISCORD_EMBED_LIMIT
 
 
-class BotCommands:
+@command(desc="List all commands")
+async def listall(ctx):
+    commands = split_dict(Commands.get_instance().dictionary, DISCORD_EMBED_LIMIT)
 
-    @command(desc="List all commands")
-    async def listall(self, ctx):
-        commands = split_dict(Commands.get_instance().dictionary, DISCORD_EMBED_LIMIT)
+    for page in commands:
+        embed = Embed(title="Commands list", color=0x00ff75)
 
-        for page in commands:
-            embed = Embed(title="Commands list", color=0x00ff75)
+        for line in page:
+            command = page[line]
+            embed.add_field(name=".{} {}".format(line, format_params(command['params'])),
+                            value=command['description'],
+                            inline=False)
 
-            for line in page:
-                command = page[line]
-                embed.add_field(name=".{} {}".format(line, format_params(command['params'])),
-                                value=command['description'],
-                                inline=False)
-
-            await ctx.author.send(embed=embed)
+        await ctx.author.send(embed=embed)
