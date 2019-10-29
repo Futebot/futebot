8c55470f130f231fa5dfad6a4b7031755d86aa33
Flávio Mendes <flavio.mendes@rockalltech.com>
2019-10-29 09:33:07 +0000

    Adding CD

--- commands/bot.py
+++ commands/bot.py
@@ -6,17 +6,19 @@ from util.helpers import format_params, split_dict
 from .config import DISCORD_EMBED_LIMIT
 
 
-@command(desc="List all commands")
-async def listall(ctx):
-    commands = split_dict(Commands.get_instance().dictionary, DISCORD_EMBED_LIMIT)
+class BotCommands:
 
-    for page in commands:
-        embed = Embed(title="Commands list", color=0x00ff75)
+    @command(desc="List all commands")
+    async def listall(self, ctx):
+        commands = split_dict(Commands.get_instance().dictionary, DISCORD_EMBED_LIMIT)
 
-        for line in page:
-            command = page[line]
-            embed.add_field(name=".{} {}".format(line, format_params(command['params'])),
-                            value=command['description'],
-                            inline=False)
+        for page in commands:
+            embed = Embed(title="Commands list", color=0x00ff75)
 
-        await ctx.author.send(embed=embed)
+            for line in page:
+                command = page[line]
+                embed.add_field(name=".{} {}".format(line, format_params(command['params'])),
+                                value=command['description'],
+                                inline=False)
+
+            await ctx.author.send(embed=embed)
