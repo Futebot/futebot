f265b8bc3068b8e0905828d838a374cc54003e5b
Mendes <Flavio.Mendes@broadridge.com>
2020-01-23 14:17:50 +0000

    refactor

--- main_slack.py
+++ main_slack.py
@@ -113,16 +113,15 @@ def handle_command(command, channel):
 
 
 if __name__ == "__main__":
-    # if slack_client.rtm_connect(with_team_state=False):
-    #     print("Starter Bot connected and running!")
-    #     # Read bot's user ID by calling Web API method `auth.test`
-    #     starterbot_id = slack_client.api_call("auth.test")["user_id"]
-    #     while True:
-    #         command, channel = parse_bot_commands(slack_client.rtm_read())
-    #         if command:
-    #             handle_command(command, channel)
-    #         time.sleep(RTM_READ_DELAY)
-    # else:
-    #     print("Connection failed. Exception traceback printed above.")
+    if slack_client.rtm_connect(with_team_state=False):
+        print("Starter Bot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        starterbot_id = slack_client.api_call("auth.test")["user_id"]
+        while True:
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
+    else:
+        print("Connection failed. Exception traceback printed above.")
 
-    handle_command(".18k", "channel")
